dd95f4b707e35848904b686a2b2e75cd91098abf
Konstantin Gukov <gukoff@yandex-team.ru>
2018-05-10 11:16:46 -0400

    Handle open issues

--- run_service.py
+++ run_service.py
@@ -1,13 +1,42 @@
+import aiohttp
 from aiohttp import web
+from gidgethub import routing, sansio
+from gidgethub.aiohttp import GitHubAPI
 
 import settings
 
+router = routing.Router()
+
+
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """ Whenever an issue is opened, greet the author and say thanks."""
+    author = event.data["issue"]["user"]["login"]
+    await gh.post(
+        event.data["issue"]["comments_url"],
+        data={
+            'body': f'Thanks for the report, @{author}! But I am just a :robot:...'
+        }
+    )
+
 
 async def main(request):
-    return web.Response(status=200, text="Hello world!")
+    event = sansio.Event.from_http(
+        body=await request.read(),
+        headers=request.headers,
+        secret=settings.WEBHOOK_SECRET,
+    )
+
+    async with aiohttp.ClientSession() as session:
+        gh = GitHubAPI(
+            session, 'gukoff', oauth_token=settings.TOKEN
+        )
+        await router.dispatch(event, gh)
+
+    return web.Response(status=200)
 
 
 if __name__ == "__main__":
     app = web.Application()
-    app.router.add_get("/", main)
+    app.router.add_post("/", main)
     web.run_app(app, port=settings.APP_PORT)
--- settings.py
+++ settings.py
@@ -2,4 +2,6 @@ import os
 
 # acquired at https://github.com/settings/tokens
 TOKEN = os.getenv('GH_AUTH')
+
 APP_PORT = int(os.getenv('PORT', 8080))
+WEBHOOK_SECRET = os.environ.get("GH_SECRET")
